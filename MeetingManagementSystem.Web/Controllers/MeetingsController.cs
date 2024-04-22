1bdb2b905fd2dafceee749f06d8117cf7f42af62
SerefhanDisek <glorius488@gmail.com>
2024-04-22 18:41:10 +0300

    WebUI

---
+++ MeetingManagementSystem.Web/Controllers/MeetingsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeetingManagementSystem.Web.Controllers
+{
+    public class MeetingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
